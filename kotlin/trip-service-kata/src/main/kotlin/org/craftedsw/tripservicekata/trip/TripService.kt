896fccbc835b35015a107f8f77abe7c8acb1f060
Daniele Megna <daniele.megna@xpeppers.com>
2017-03-09 14:44:57 +0100

    Added loggedUser null unwrap guard

--- kotlin/trip-service-kata/src/main/kotlin/org/craftedsw/tripservicekata/trip/TripService.kt
+++ kotlin/trip-service-kata/src/main/kotlin/org/craftedsw/tripservicekata/trip/TripService.kt
@@ -8,15 +8,13 @@ import java.util.*
 class TripService(val loggedUser: User?, val tripRepository: TripRepository) {
 
     fun getTripsByUser(user: User): List<Trip> {
-        if (loggedUser != null) {
-            for (friend in user.friends) {
-                if (friend == loggedUser)
-                    return tripRepository.findTripsFor(user)
-            }
-            return ArrayList()
-        } else {
-            throw UserNotLoggedInException()
+        var loggedUser = loggedUser.let { it } ?: throw UserNotLoggedInException()
+
+        for (friend in user.friends) {
+            if (friend == loggedUser)
+                return tripRepository.findTripsFor(user)
         }
+        return ArrayList()
     }
 
 }
