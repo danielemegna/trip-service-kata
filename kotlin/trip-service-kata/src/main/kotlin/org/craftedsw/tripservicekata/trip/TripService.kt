e01629e56144045c68c6cfa9f176f1dcb35bf1c3
Daniele Megna <daniele.megna@xpeppers.com>
2017-03-09 14:55:20 +0100

    Extracted first exit condition

--- kotlin/trip-service-kata/src/main/kotlin/org/craftedsw/tripservicekata/trip/TripService.kt
+++ kotlin/trip-service-kata/src/main/kotlin/org/craftedsw/tripservicekata/trip/TripService.kt
@@ -2,15 +2,15 @@ package org.craftedsw.tripservicekata.trip
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException
 import org.craftedsw.tripservicekata.user.User
-import org.craftedsw.tripservicekata.user.UserSession
 import java.util.*
 
 class TripService(val loggedUser: User?, val tripRepository: TripRepository) {
 
     fun getTripsByUser(user: User): List<Trip> {
         var loggedUser = loggedUser.let { it } ?: throw UserNotLoggedInException()
+        val userHasLoggedUserAsFriend = user.friends.contains(loggedUser)
 
-        if(user.friends.contains(loggedUser))
+        if (userHasLoggedUserAsFriend)
             return tripRepository.findTripsFor(user)
 
         return ArrayList()
